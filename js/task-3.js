b568b4985b5dc130150fadd19bb88c14fd1faa2d
ValeryMaz <mazhytova1996@gmail.com>
2024-12-26 13:28:43 -0500

    task 3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,11 @@
-function getElementWidth (content, padding, border){
+function getElementWidth (content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
+    
 const sum = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-return `${sum}px`; 
+
+return sum; 
 
 }
 
